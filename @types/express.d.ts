ad1b959ef54773d8892e638def561943a724abac
petarzarkov <pzarko1@gmail.com>
2025-12-26 13:29:23 +0200

    fix: build

--- @types/express.d.ts
+++ @types/express.d.ts
@@ -1,11 +1,6 @@
-import type { EnvVars } from '@/config/env-vars.dto';
 import type { SanitizedUser } from '@/users/entity/user.entity';
 
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends EnvVars {}
-  }
-
   namespace Express {
     interface User extends SanitizedUser {}
     interface Locals {
